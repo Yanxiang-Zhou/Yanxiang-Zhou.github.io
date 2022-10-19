f6e765a283765806f69fdffec7fb81678f09e532
Yanxiang Zhou <94133385+Yanxiang-Zhou@users.noreply.github.com>
2022-10-19 17:26:33 -0400

    Update GitProfile.jsx

--- src/components/GitProfile.jsx
+++ src/components/GitProfile.jsx
@@ -60,7 +60,7 @@ const GitProfile = ({ config }) => {
           name: data.name ? data.name : '',
           bio: data.bio ? data.bio : '',
           location: data.location ? data.location : '',
-          company: data.company ? data.company : '',
+          institution: data.company ? data.company : '',
         };
 
         setProfile(profileData);
